6ee1d16b476997443d76b3514cfdae846d8b6acc
niles87 <nbing19@gmail.com>
2020-04-23 06:05:09 -0700

    added jsx export and delete function for page functionality

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,14 +1,16 @@
 import React, { Component } from "react";
-import API from "../utils/API";
-import Container from "../components/Container";
+
+import { Card, CardBody, CardFooter, CardImage } from "../components/Card";
 import { List, ListItem } from "../components/List";
+import Container from "../components/Container";
+import Display from "../components/Display";
+import Button from "../components/Button";
+import Image from "../components/Image";
+import API from "../utils/API";
 
 class Saved extends Component {
   state = {
     books: [],
-    title: "",
-    author: "",
-    synopsis: "",
   };
 
   componentDidMount() {
@@ -17,17 +19,56 @@ class Saved extends Component {
 
   loadBooks = () => {
     API.getBooks()
-      .then((res) => this.setState({ books: res.data, title: "", author: "", synopsis: "" }))
+      .then((res) => {
+        this.setState({ books: res.data });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  handleDelete = (id) => {
+    API.deleteBook(id)
+      .then((res) => this.loadBooks())
       .catch((err) => console.log(err));
   };
 
   render() {
     return (
-      <Container>
-        <List>
-          <ListItem></ListItem>
-        </List>
-      </Container>
+      <>
+        <Display>
+          <h1>Saved Books</h1>
+        </Display>
+        <Container>
+          {this.state.books.length ? (
+            <List>
+              {this.state.books.map((book) => (
+                <ListItem key={book._id}>
+                  <Card>
+                    <CardImage>
+                      <Image src={book.image ? book.image : ""} alt={book.title} />
+                    </CardImage>
+                    <div>
+                      <CardBody>
+                        <h4>
+                          {book.title} by {book.authors}
+                        </h4>
+                        <p>{book.description}</p>
+                      </CardBody>
+                      <CardFooter>
+                        <a href={book.link} target="_blank" rel="noopener noreferrer">
+                          preview
+                        </a>
+                        <Button onClick={() => this.handleDelete(book._id)} />
+                      </CardFooter>
+                    </div>
+                  </Card>
+                </ListItem>
+              ))}
+            </List>
+          ) : (
+            <h2>No Saved Books</h2>
+          )}
+        </Container>
+      </>
     );
   }
 }
